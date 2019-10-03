f18df98b8783b561dc53280c015af18c0cdcf67c
Serdar Tumgoren <zstumgoren@gmail.com>
2019-10-03 16:19:28 -0700

    Fix integrate command for user with no org affilitions #15
    - Apply fix for basic user scenario
    - Refactor Integrate command which had grown quite hairy

--- datakit_github/commands/integrate.py
+++ datakit_github/commands/integrate.py
@@ -29,61 +29,70 @@ class Integrate(ProjectMixin, Command):
                 "configure Github integration.\n"
             )
         else:
-            accounts = GithubApi.accounts(api_key)
-            # Prompt user to select an account.
-            if len(accounts) == 1:
-                account = accounts[0]
-            else:
-                msg = "Choose an account where the new project should be created:\n"
-                account_lkup = {}
-                self.log.info(msg)
-                for idx, account in enumerate(accounts):
-                    num = idx + 1
-                    account_lkup[num] = account
-                    self.log.info("({}) {}".format(num, account.login))
-
-                # TODO: Check plugin for default account configuration,
-                # otherwise default to personal account
-                default = account_lkup[1]
-                choice_msg = "\nType a number or leave blank for default [{}]: ".format(default.login)
-                choice = ask(choice_msg)
-                if choice.strip() == '':
-                    target_account = default
-                else:
-                    target_account = account_lkup[int(choice)]
-                self.log.info("You chose: {}".format(target_account.login))
-                confirm = ask("Is this correct? y/n [y]: ").strip().lower()
-                if confirm in ["y", ""]:
-                    # TODO: Handle overrides for project settings from
-                    # configs and/or command-line flags (e.g. privacy)
-                    privacy_choice = ask("Should this repo be private? y/n [y]: ").strip().lower()
-                    privacy = True if privacy_choice in ['', 'y'] else False
+            account = self.choose_account(api_key)
+            confirm = self.confirm_account_choice(account)
+            if confirm in ["y", ""]:
+                # TODO: Handle overrides for project settings from
+                # configs and/or command-line flags (e.g. privacy)
+                privacy_choice = ask("Should this repo be private? y/n [y]: ").strip().lower()
+                privacy = True if privacy_choice in ['', 'y'] else False
+                try:
+                    repo = account.create_repo(self.project_slug, private=privacy)
+                    self.log.info("Repo created at {}".format(repo.html_url))
+                    self.run_local_git_commands(repo)
+                except GithubException as err:
                     try:
-                        resp = target_account.create_repo(self.project_slug, private=privacy)
-                        self.log.info("Repo created at {}".format(resp.html_url))
-                        self.log.info("Running local Git initialization...")
-                        # TODO: Create a project-level config/datakit-github.json?
-                        # containing name of selected account and possibly account type (org or user)?
-                        # This can be used downstream to configure org or user-specific API calls
-                        # if any (hold off on the "type" config until we
-                        # determine if there are different call ypes)
-                        Repository.init()
-                        Repository.add()
-                        Repository.commit("Initial commit")
-                        Repository.add_remote(resp.ssh_url)
-                        alert_msg = 'Local repo linked to remote origin: \n\t{}'.format(resp.html_url)
-                        self.log.info(alert_msg)
-                        Repository.push()
-                        self.log.info("First commit made locally and pushed to remote")
-                        self.log.info("View the project on Github at {}".format(resp.html_url))
-                    except GithubException as err:
-                        try:
-                            error_msg = err.data['errors'][0]['message']
-                        except KeyError:
-                            error_msg = err.data['message']
-                        msg = "\nERROR: Failed to create {} for {}: {}!!\n".format(
-                            self.project_slug,
-                            target_account.login,
-                            error_msg
-                            )
-                        self.log.error(msg)
+                        error_msg = err.data['errors'][0]['message']
+                    except KeyError:
+                        error_msg = err.data['message']
+                    msg = "\nERROR: Failed to create {} for {}: {}!!\n".format(
+                        self.project_slug,
+                        account.login,
+                        error_msg
+                        )
+                    self.log.error(msg)
+
+    def choose_account(self, api_key):
+        accounts = GithubApi.accounts(api_key)
+        if len(accounts) == 1:
+            target_account = accounts[0]
+        else:
+            msg = "Choose an account where the new project should be created:\n"
+            account_lkup = {}
+            self.log.info(msg)
+            for idx, account in enumerate(accounts):
+                num = idx + 1
+                account_lkup[num] = account
+                self.log.info("({}) {}".format(num, account.login))
+            # TODO: Check plugin for default account configuration,
+            # otherwise default to personal account
+            default = account_lkup[1]
+            choice_msg = "\nType a number or leave blank for default [{}]: ".format(default.login)
+            choice = ask(choice_msg)
+            if choice.strip() == '':
+                target_account = default
+            else:
+                target_account = account_lkup[int(choice)]
+        return target_account
+
+    def confirm_account_choice(self, target_account):
+        self.log.info("Repo will be created on account: {}".format(target_account.login))
+        choice = ask("Is this correct? y/n [y]: ").strip().lower()
+        return choice
+
+    def run_local_git_commands(self, repo):
+        self.log.info("Running local Git initialization...")
+        # TODO: Create a project-level config/datakit-github.json?
+        # containing name of selected account and possibly account type (org or user)?
+        # This can be used downstream to configure org or user-specific API calls
+        # if any (hold off on the "type" config until we
+        # determine if there are different call ypes)
+        Repository.init()
+        Repository.add()
+        Repository.commit("Initial commit")
+        Repository.add_remote(repo.ssh_url)
+        alert_msg = 'Local repo linked to remote origin: \n\t{}'.format(repo.html_url)
+        self.log.info(alert_msg)
+        Repository.push()
+        self.log.info("First commit made locally and pushed to remote")
+        self.log.info("View the project on Github at {}".format(repo.html_url))
