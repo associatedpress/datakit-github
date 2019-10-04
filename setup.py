b3fa02aa18de6d9fc995ecb2318f2a5a5d050850
Serdar Tumgoren <zstumgoren@gmail.com>
2019-10-04 11:44:31 -0700

    Fix setup.py to avoid PyPI deployment breakage

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+datakit-github
+---------------
+
+This plugin provides light-weight GitHub integration for the `DataKit_ command-line tool <https::/datakit.ap.org>`_.
+
+It's intended to streamline the process of generating a GitHub project and linking it to a local
+directory containing a newly created project skeleton.
+
+DataKit is a project by The Associated Press designed to streamline data analysis workflows.
+To learn more about the wider DataKit ecosystem and how AP uses it, check out the
+`official documentation for DataKit <https://datakit.ap.org>`_.
+
+More information on the datakit-github plugin can be found below:
+
+* Documentation: http://datakit-github.readthedocs.io/en/latest/
+* GitHub: https://github.com/associatedpress/datakit-github
+* PyPI: https://pypi.python.org/pypi/datakit-github
+* Free and open source software: `ISC license  <https://github.com/associatedpress/datakit-github/blob/master/LICENSE>`_
+
+"""
+
 from setuptools import setup
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = [
     'cliff',
@@ -26,7 +42,7 @@ setup(
     name='datakit-github',
     version='0.1.0',
     description="Light-weight GitHub integration for datakit workflows.",
-    long_description=readme + '\n\n' + history,
+    long_description=__doc__,
     author="Serdar Tumgoren",
     author_email='zstumgoren@gmail.com',
     url='https://github.com/associatedpress/datakit-github',
@@ -46,14 +62,12 @@ setup(
     zip_safe=False,
     keywords='datakit-github',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
