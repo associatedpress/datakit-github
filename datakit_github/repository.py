a56fdd64f48dcff0f006bdd7da632fdc5372ef34
Justin Myers <jmyers@ap.org>
2022-07-19 14:45:15 -0500

    Finish the switch from `master` to `main`
    The change in 6358fb3d25e80c188a56c0f1f1a8de334c0b5ba7 assumes the user
    is on a Git version recent enough to have its default branch be `main`
    (or that the user's Git configuration otherwise provides this default).
    As discussed in associatedpress/datakit-gitlab#8, we now ensure the
    current (newly created) branch is called `main` regardless of the
    configured default.

--- datakit_github/repository.py
+++ datakit_github/repository.py
@@ -25,5 +25,9 @@ class Repository:
         return subprocess.check_output(['git', 'remote', 'add', name, repo_url])
 
     @staticmethod
+    def rename_current_branch(new_name):
+        return subprocess.check_output(['git', 'branch', '--move', new_name])
+
+    @staticmethod
     def push(remote='origin', branch='main'):
         return subprocess.check_output(['git', 'push', '-u', remote, branch])
