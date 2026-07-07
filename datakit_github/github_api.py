da66f3f97385a1445bdc0fe6a8374e6ac6e6a20f
Larry Fenn <lfenn@ap.org>
2026-07-07 13:45:20 -0400

    Fix deprecated Github auth method

--- datakit_github/github_api.py
+++ datakit_github/github_api.py
@@ -1,12 +1,12 @@
 from operator import attrgetter
-from github import Github
+from github import Auth, Github
 
 
 class GithubApi:
 
     @staticmethod
     def accounts(api_key):
-        g = Github(api_key)
+        g = Github(auth=Auth.Token(api_key))
         user = g.get_user()
         accounts = [user]
         orgs = [org for org in user.get_orgs()]
